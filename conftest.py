2e3ef2e42988ec4976cb76fcaa1927bedc16cd7c
Jason R. Coombs <jaraco@jaraco.com>
2024-05-28 12:21:19 -0400

    Patch lzma and bz2 modules to allow tests to pass.

--- conftest.py
+++ conftest.py
@@ -22,6 +22,36 @@ def backport_as_std():
     sys.modules['tarfile'] = tarfile
 
 
+def patch_bz2():
+    """
+    Ensure bz2.BZ2File reflects the name.
+    """
+    if sys.version_info > (3, 13):
+        return
+    try:
+        import bz2
+    except ImportError:
+        return
+
+    bz2.BZ2File.name = property(lambda self: self._fp.name)
+
+
+def patch_lzma():
+    """
+    Ensure lzma.LZMAFile reflects the name.
+    """
+    if sys.version_info > (3, 13):
+        return
+    try:
+        import lzma
+    except ImportError:
+        return
+
+    lzma.LZMAFile.name = property(lambda self: self._fp.name)
+
+
 def pytest_configure():
     patch_findfile()
     backport_as_std()
+    patch_bz2()
+    patch_lzma()
